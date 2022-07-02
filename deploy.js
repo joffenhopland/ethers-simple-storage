c2b4038c88a03301b6421f5118766a8fb96a6e1e
Jon-Fredrik Hopland <jho107@post.uit.no>
2022-07-02 22:38:52 +0200

    successfully deployed to rinkeby test net

--- deploy.js
+++ deploy.js
@@ -3,14 +3,14 @@ const fs = require("fs-extra")
 require("dotenv").config()
 
 async function main() {
-    const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL)
-    // const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
-    const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf8")
-    let wallet = new ethers.Wallet.fromEncryptedJsonSync(
-        encryptedJson,
-        process.env.PRIVATE_KEY_PASSWORD
-    )
-    wallet = await wallet.connect(provider)
+    let provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL)
+    let wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
+    // const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf8")
+    // let wallet = new ethers.Wallet.fromEncryptedJsonSync(
+    //     encryptedJson,
+    //     process.env.PRIVATE_KEY_PASSWORD
+    // )
+    // wallet = await wallet.connect(provider)
     const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8")
     const binary = fs.readFileSync(
         "./SimpleStorage_sol_SimpleStorage.bin",
@@ -19,7 +19,9 @@ async function main() {
     const contractFactory = new ethers.ContractFactory(abi, binary, wallet)
     console.log("Deploying, please wait...")
     const contract = await contractFactory.deploy()
-    const transactionReceipt = await contract.deployTransaction.wait(1)
+    await contract.deployTransaction.wait(1)
+    console.log(`Contract Address: ${contract.address}`)
+
     // console.log("Deployment transaction (transaction response): ");
     // console.log(contract.deployTransaction);
     // console.log("Transaction receipt: ");
@@ -42,12 +44,13 @@ async function main() {
     // console.log(sentTxResonse);
 
     // get number
-    const currentFavoriteNumber = await contract.retrieve()
-    console.log(`currentFavoriteNumber: ${currentFavoriteNumber}`)
-    const transactionResponse = await contract.store("55")
-    const transactionReceived = await transactionResponse.wait(1)
-    const updatedFavoriteNumber = await contract.retrieve()
-    console.log(`Updated favorite number is: ${updatedFavoriteNumber}`)
+    let currentFavoriteNumber = await contract.retrieve()
+    console.log(`Current Favorite Number: ${currentFavoriteNumber}`)
+    console.log("Updating favorite number...")
+    let transactionResponse = await contract.store(7)
+    let transactionReceipt = await transactionResponse.wait()
+    currentFavoriteNumber = await contract.retrieve()
+    console.log(`New Favorite Number: ${currentFavoriteNumber}`)
 }
 
 main()
